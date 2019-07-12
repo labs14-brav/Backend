5b80d6e55eeeb2e467a5df9f1e3ff1c2f8c8ff73
mjfalahee <mjfalahee@gmail.com>
2019-07-11 18:36:43 -0700

    Added a random number to usernames in order to avoid duplicates

--- data/fakeUserData.js
+++ data/fakeUserData.js
@@ -5,7 +5,7 @@ let typearray = ['User', 'Mediator', 'Admin'];
 for (i = 0; i < 500; i++) {
   let rN = Math.floor(Math.random() * typearray.length);
   seeds[i] = {
-    username: faker.name.findName(),
+    username: `${faker.name.lastName}${Math.floor(Math.random()*200)}`,
     type: typearray[rN],
     email: faker.internet.email(),
     nickname: faker.name.lastName(),
