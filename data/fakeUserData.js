532aee3ae7f8c5bf8ea43441d4adc41c11544565
mjfalahee <mjfalahee@gmail.com>
2019-07-11 18:27:48 -0700

    Working on figuring out seeds still. Adding console logs to look at this in detail

--- data/fakeUserData.js
+++ data/fakeUserData.js
@@ -1,5 +1,4 @@
 var faker = require('faker');
-faker.seed(1);
 
 let seeds = [];
 let typearray = ['User', 'Mediator', 'Admin'];
@@ -14,24 +13,24 @@ for (i = 0; i < 500; i++) {
 };
 
 console.log('fake users are populating seeds ======')
-console.log(seeds[0]);
-console.log(seeds[499]);
+// console.log(seeds[0]);
+// console.log(seeds[499]);
 
 let seeds1 = seeds.slice(99);
 // console.log('seeds1 ==========');
 // console.log(seeds1[0], seeds1[99]);
 let seeds2 = seeds.slice(100, 199);
-// console.log('seeds2 ==========');
-// console.log(seeds2[0], seeds2[98]);
+ console.log('seeds2 ==========');
+ console.log(seeds2[0], seeds2[98]);
 let seeds3 = seeds.slice(200, 299);
-// console.log('seeds3 ==========');
-// console.log(seeds3[0], seeds3[98]);
+ console.log('seeds3 ==========');
+ console.log(seeds3[0], seeds3[98]);
 let seeds4 = seeds.slice(300, 399);
-// console.log('seeds4 ==========');
-// console.log(seeds4[0], seeds4[98]);
+ console.log('seeds4 ==========');
+ console.log(seeds4[0], seeds4[98]);
 let seeds5 = seeds.slice(400, 499);
-// console.log('seeds5 ==========');
-// console.log(seeds5[0], seeds5[98]);
+ console.log('seeds5 ==========');
+ console.log(seeds5[0], seeds5[98]);
 
 
 module.exports = {
