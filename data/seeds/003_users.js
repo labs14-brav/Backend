88f233064552dd740b209d59387b131bcbb11d69
mjfalahee <mjfalahee@gmail.com>
2019-07-11 18:29:42 -0700

    More console logs to see where things are going wrong

--- data/seeds/003_users.js
+++ data/seeds/003_users.js
@@ -1,5 +1,8 @@
 const data = require('../fakeUserData');
 
+
+console.log('SEEDS 3 ============', data.seeds3[50]);
+
 exports.seed = function(knex) {
   return knex('users').insert(data.seeds3);
 };
--- data/seeds/005_users.js
+++ data/seeds/005_users.js
@@ -1,5 +1,8 @@
 const data = require('../fakeUserData');
 
+
+console.log('SEEDS 5 ============', data.seeds5[50]);
+
 exports.seed = function(knex) {
   return knex('users').insert(data.seeds5);
 };
