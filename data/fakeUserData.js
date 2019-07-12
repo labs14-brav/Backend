c82ff053f1ee21d3baa4fb8c9ddf59fb34c278e7
mjfalahee <mjfalahee@gmail.com>
2019-07-11 18:38:10 -0700

    Fixed faker lastname()

--- data/fakeUserData.js
+++ data/fakeUserData.js
@@ -5,10 +5,10 @@ let typearray = ['User', 'Mediator', 'Admin'];
 for (i = 0; i < 500; i++) {
   let rN = Math.floor(Math.random() * typearray.length);
   seeds[i] = {
-    username: `${faker.name.lastName}${Math.floor(Math.random()*200)}`,
+    username: `${faker.name.lastName()}${Math.floor(Math.random()*200)}`,
     type: typearray[rN],
     email: faker.internet.email(),
-    nickname: faker.name.lastName(),
+    nickname: faker.name.firstName(),
   };
 };
 
