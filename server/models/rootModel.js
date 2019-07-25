772e66cbec89dfb1d4f0a8840817cdfbd46164bd
Andy Bettisworth <andy@accreu.com>
2019-07-25 17:00:34 -0500

    Handle staging environment

--- server/models/rootModel.js
+++ server/models/rootModel.js
@@ -23,18 +23,17 @@ function getUserById(id) {
 };
 
 async function addUser(user) {
-    if(process.env.NODE_ENV=="production"){
-        const [ids] = await db('users').insert({
-            email: user.email,
-            uid: user.uid,
-        },['id'])
-        return ids.id
-    }else{
-        const [id] = await db('users').insert({
-            email: user.email,
-            uid: user.uid,
-        })
-        return id
-    }
-
+  if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV === 'staging') {
+    const [ids] = await db('users').insert({
+        email: user.email,
+        uid: user.uid,
+    },['id'])
+    return ids.id
+  } else {
+    const [id] = await db('users').insert({
+        email: user.email,
+        uid: user.uid,
+    })
+    return id
+  }
 };
