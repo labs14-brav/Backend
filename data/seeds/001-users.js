6fe05362980de2c2617f51a05ac08cf5c4cedcf5
Andy Bettisworth <andy@accreu.com>
2019-07-26 13:16:14 -0500

    Add temp mediator account for testing

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -6,6 +6,7 @@ exports.seed = function(knex) {
       return knex('users').insert([
         {id: 1, type: 'user', email: 'labs14brav-user@gmail.com', uid: 'crGiJFUlI4XxxbXC6F1wQ8gDn983'}, // password: secret
         {id: 2, type: 'admin', email: 'labs14brav-admin@gmail.com', uid: 'rkYJqWNwHVb06XFszYhQOkuU5h42'}, // password: secret
+        {id: 3, type: 'mediator', email: 'labs14brav-mediator@gmail.com', uid: '3VhJYYOASWPcC499nsrjWM1SCxF3'}, // password: secret
       ]);
     });
 };
