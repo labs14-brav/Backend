d7b2cd609f559037dd19ad937269f20d6f2e8a59
Andy Bettisworth <andy@accreu.com>
2019-07-26 13:12:31 -0500

    Add comment with password for temp accounts

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,8 +4,8 @@ exports.seed = function(knex) {
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        {id: 1, type: 'user', email: 'labs14brav-user@gmail.com', uid: 'crGiJFUlI4XxxbXC6F1wQ8gDn983'},
-        {id: 2, type: 'admin', email: 'labs14brav-admin@gmail.com', uid: 'rkYJqWNwHVb06XFszYhQOkuU5h42'},
+        {id: 1, type: 'user', email: 'labs14brav-user@gmail.com', uid: 'crGiJFUlI4XxxbXC6F1wQ8gDn983'}, // password: secret
+        {id: 2, type: 'admin', email: 'labs14brav-admin@gmail.com', uid: 'rkYJqWNwHVb06XFszYhQOkuU5h42'}, // password: secret
       ]);
     });
 };
