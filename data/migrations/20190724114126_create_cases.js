6ebebbd79e5eb467635b1bccd30fe05a1411a7e0
mjfalahee <mjfalahee@gmail.com>
2019-07-30 14:22:48 -0700

    Edited cases migration for new changes to schema

--- data/migrations/20190724114126_create_cases.js
+++ data/migrations/20190724114126_create_cases.js
@@ -7,7 +7,15 @@ exports.up = function(knex) {
     table.text('user_uid');
     table.text('description');
     table.text('dispute_category');
+    table.text('dispute_amount');
     table.text('parties_involved');
+    table.text('parties_contact_info');
+    table.boolean('court_case'); //this is the important check for whether or not a case is a court referral
+    table.text('court_jurisdiction');
+    table.text('court_number');
+    table.text('court_filing_date');
+    table.text('court_case_type');
+    table.text('case_notes');
   })
 };
 
