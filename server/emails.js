9b1f444c791d2b2f98b56512c1c2c1674eb9216a
Brennon Nuckols <brennuck@gmail.com>
2019-07-30 13:49:01 -0600

    installed dependicies and made emails file

---
+++ server/emails.js
@@ -0,0 +1,8 @@
+const SGmail = require('@sendgrid/mail')
+
+SGmail.setApiKey('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')
+
+
+// echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+// echo "sendgrid.env" >> .gitignore
+// source ./sendgrid.env
