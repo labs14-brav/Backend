6903dc0fdd4c7d036e742c30340049649c5da380
Andy Bettisworth <andy@accreu.com>
2019-08-01 17:07:12 -0500

    Conditionally filter mediators

--- server/models/Mediator.js
+++ server/models/Mediator.js
@@ -15,31 +15,47 @@ class Mediator {
     const { price, language, specialty, experience } = query
     let price_min, price_max
 
+    let has_filters = false
     let filter = {}
 
-    if (language) filter['language'] = language
-    if (specialty) filter['specialization'] = specialty
-    if (experience) filter['experience'] = experience
+    if (language) {
+      filter['language'] = language
+      has_filters = true
+    }
+
+    if (specialty) {
+      filter['specialization'] = specialty
+      has_filters = true
+    }
+
+    if (experience) {
+      filter['experience'] = experience
+      has_filters = true
+    }
 
     if (price == '<25') {
       price_min = 0
       price_max = 25
+      has_filters = true
     } else if (price == '25-75') {
       price_min = 25
       price_max = 75
+      has_filters = true
     } else if (price == '>75') {
       price_min = 75
       price_max = 99999
+      has_filters = true
     } else {
       price_min = 0
       price_max = 99999
     }
 
-    const results = db('users').where('type', 'mediator')
-      .where(filter).whereBetween('price', [price_min, price_max])
-
-    return db('users').where('type', 'mediator')
-      .where(filter).whereBetween('price', [price_min, price_max])
+    if (has_filters) {
+      return db('users').where('type', 'mediator')
+        .where(filter).whereBetween('price', [price_min, price_max])
+    } else {
+      return db('users').where('type', 'mediator')
+    }
   }
 
   static find(id) {
