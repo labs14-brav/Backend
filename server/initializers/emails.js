79e83b95eca7f5fbd148316e006e83ebe41af133
mjfalahee <mjfalahee@gmail.com>
2019-08-13 15:44:11 -0700

    Changed out sample email with a transactional template I created

--- server/initializers/emails.js
+++ server/initializers/emails.js
@@ -4,15 +4,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 function sendMail(info) {
     const message = {
         to: info.mediator_email,
-        from: 'labs14brav@gmail.com',
+        from: 'labs14brav@beabravone.com',
         subject: "Mediator Request!",
-        text: `
-        You've been requested as a mediator!
-    
-        User email: ${info.user_email}
-        Dispute Category: ${info.dispute_category}
-    
-        Login for more info`,
+        text: "Email template failed",
+        template_id:"d-16b1a85dd5554c30a211f12b8d535db0"
     };
 
     sgMail.send(message);
