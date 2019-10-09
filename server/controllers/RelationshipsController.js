295235ae345ad04a3efe6744b3a7cbee74217b68
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-10-09 17:35:51 -0400

    Added the controllers for returning all relationships, and for returning a relationship by id.

--- server/controllers/RelationshipsController.js
+++ server/controllers/RelationshipsController.js
@@ -8,7 +8,38 @@ const Relationships = require("../models/Relationships");
  * Define controller
  */
 
-class RelationshipsController {}
+class RelationshipsController {
+  static async all(req, res) {
+    try {
+      const relationships = await Relationships.find();
+
+      return res.status(200).json(relationships);
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: { message: "Internal Server Error" } });
+    }
+  }
+
+  static async findById(req, res) {
+    const { id } = req.params;
+
+    try {
+      const relationship = await Relationships.findById(id);
+      if (relationship) {
+        return res.status(200).json(relationship);
+      } else {
+        //404
+      }
+    } catch (error) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: { message: "Internal Server Error" } });
+    }
+  }
+}
 
 /**
  * Export controller
