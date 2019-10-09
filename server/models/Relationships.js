fc72d7982dc3ea070caae2e502a614eef37e58a5
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-10-09 17:20:53 -0400

    Added new file for Relationships model. Added class Relationships to the file, and left it empty for now.

---
+++ server/models/Relationships.js
@@ -0,0 +1,17 @@
+/**
+ * Dependencies
+ */
+
+const db = require("../../data/dbConfig");
+
+/**
+ * Define model
+ */
+
+class Relationships {}
+
+/**
+ * Export model
+ */
+
+module.exports = Relationships;
