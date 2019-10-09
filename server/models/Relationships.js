f9037d261e3dc4feb450f5160ef92bda58378670
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-10-09 17:23:24 -0400

    Added a create helper method, which will create a new row in the Relationships table using the user_id and mediator_id. Accounted for whether or not the queries would be run in a postgreSQL or SQLite environment.

--- server/models/Relationships.js
+++ server/models/Relationships.js
@@ -18,6 +18,28 @@ class Relationships {
       .where({ id })
       .first();
   }
+
+  static async create(user_id, mediator_id) {
+    if (
+      process.env.NODE_ENV === "production" ||
+      process.env.NODE_ENV === "staging"
+    ) {
+      const relationship = await db("relationships")
+        .insert({ user_id, mediator_id })
+        .returning("*");
+
+      // postgreSQL returns the object inside of an object. This will return the med object that we want.
+      return relationship["0"];
+    } else {
+      const [id] = await db("relationships").insert({ user_id, mediator_id });
+      if (id) {
+        const new_relationship = await db("relationships")
+          .where({ id })
+          .first();
+        return new_relationship;
+      }
+    }
+  }
 }
 
 /**
